405b72488ed89913111635999808616bd9446d12
Rajan Dhaliwal <rajandhaliwal80@gmail.com>
2019-04-06 23:40:10 -0400

    Added angular to express

--- server/config/app.js
+++ server/config/app.js
@@ -104,6 +104,9 @@ app.use('/api', contactRouter);
 app.use('/api/contact-list', contactsRouter);
 app.use('/api', projectRouter);
 app.use('/api', servicesRouter);
+app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, '../../public/index.html'));
+});
 
 
 // catch 404 and forward to error handler
